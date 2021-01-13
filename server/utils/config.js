6c2604e8ddc4dae4c31a6c97f91733566939e8d5
Perry Olsson <olsson.perry@gmail.com>
2021-01-13 10:23:21 -0800

    added dev port environment variable

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 
-let PORT = process.env.SERVER_PORT;
+let PORT =
+  process.env.NODE_ENV === "development"
+    ? process.env.DEV_PORT
+    : process.env.PORT;
 let MONGODB_URI = process.env.MONGODB_URI;
 
 if (process.env.NODE_ENV === "test") MONGODB_URI = process.env.MONGODB_URI_TEST;
